f64a784ee036e32fe482528074f0785ccf03ac5b
Pavel Krutov <pavel@krutov.eu>
2026-01-11 20:03:06 +0000

    refactor: use Python On Whales to manage docker stuff

--- src/docker_callback.py
+++ src/docker_callback.py
@@ -1,57 +1,52 @@
 import json
-import subprocess
 import logging
 
 from webhook import send_webhook
+from python_on_whales import DockerException, docker
 
 logger = logging.getLogger(__name__)
 
 def callback(message):
-    """Process received Pub/Sub messages"""
+    """
+    Processes receives Pub/Sub Messages and updates containers
+
+    :param message:
+    :return:
+    """
+    message_data = json.loads(message.data.decode('utf-8'))
+
+    compose_name = message_data.get("compose-name")
+    image = message_data.get("image")
+
+    if not compose_name:
+        logger.error(f"Missing compose-name in message {message_data}")
+        message.ack()
+        return
+
     try:
-        # Parse the message
-        logger.info(f"Received message: {message.data.decode('utf-8')}")
-        message_data = json.loads(message.data.decode('utf-8'))
-
-        compose_name = message_data.get("compose-name")
-        image = message_data.get("image")
-
-        if not compose_name:
-            logger.error("Missing compose-name in message")
-            message.ack()
-            return
-
         logger.info(f"Starting deployment for: {compose_name}")
         logger.info(f"Image: {image}")
 
-        # Send start notification
         send_webhook("start", compose_name, image)
 
-        # Step 1: Pull the compose service
-        logger.info("Pulling Docker Compose service...")
-        subprocess.run(["sudo", "docker", "compose", "pull", compose_name], check=True)
+        logger.info("Pulling latest Docker image...")
+        docker.compose.pull(compose_name)
 
-        # Step 2: Start the service
-        logger.info("Starting Docker Compose service...")
-        subprocess.run(["sudo", "docker", "compose", "up", compose_name, "-d"], check=True)
+        logger.info("Restarting Docker Compose service...")
+        docker.compose.up(compose_name, detach=True)
 
-        # Step 3: Clean up
-        logger.info("Cleaning up unused Docker resources...")
-        subprocess.run(["sudo", "docker", "system", "prune", "-f"], check=True)
+        logger.info("Pruning the Docker system...")
+        docker.system.prune()
 
-        # Step 4: Send Discord notification
         send_webhook("success", compose_name, image)
 
         # Acknowledge the message
         message.ack()
         logger.info(f"Message acknowledged: {message.message_id}")
 
-    except subprocess.CalledProcessError as e:
-        send_webhook("failure", compose_name, image, str(e))
-        logger.error(f"Command failed: {e}")
-        message.nack()
+    except DockerException as e:
+        error_message = f"Docker Exit Code {e.return_code} while running {e.docker_command}. Error: {e.stderr}"
 
-    except Exception as e:
-        send_webhook("failure", compose_name, image, str(e))
-        logger.error(f"Error processing message: {e}")
+        send_webhook("failure", compose_name, image, error_message)
+        logger.error(error_message)
         message.nack()
